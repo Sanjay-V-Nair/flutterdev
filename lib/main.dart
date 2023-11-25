8c7070e11159ebf85547a7a9777805d547f67bf4
Sanjay-V-Nair <sanjay.v2021@vitbhopal.ac.in>
2023-11-25 23:08:47 +0530

    Main file modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Text("Hello World!"),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 44, 14, 94),
+                Color.fromARGB(255, 84, 38, 163),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello World!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
